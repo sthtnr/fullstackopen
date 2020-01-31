2d52daaf01da3a39e7f21e98d0a1f9e9ca3658c5
sthtnr <sthtnr@gmail.com>
2020-01-31 16:02:25 +0900

    separate route handler and mongoDB config

--- part4/Blog_list/index.js
+++ part4/Blog_list/index.js
@@ -3,17 +3,9 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const mongoose = require('mongoose')
 const config = require('./utils/config')
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const blogsRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
 
 mongoose.connect(config.MONGOURL, {
   useNewUrlParser: true,
@@ -21,20 +13,7 @@ mongoose.connect(config.MONGOURL, {
 
 app.use(cors())
 app.use(bodyParser.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then(blogs => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then(result => {
-    response.status(201).json(result)
-  })
-})
+app.use('/api/blogs', blogsRouter)
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
