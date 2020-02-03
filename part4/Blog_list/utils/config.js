9fce9cf0f11323ffd0ac9fd68855d4b7e6cc85a2
sthtnr <sthtnr@gmail.com>
2020-02-03 13:54:24 +0900

    ✅ part4 step8

--- part4/Blog_list/utils/config.js
+++ part4/Blog_list/utils/config.js
@@ -3,4 +3,8 @@ require('dotenv').config()
 let PORT = process.env.PORT
 let MONGOURL = process.env.MONGOURL
 
+if (process.env.NODE_ENV === 'test') {
+  MONGOURL = process.env.TEST_MONGOURL
+}
+
 module.exports = { PORT, MONGOURL }
