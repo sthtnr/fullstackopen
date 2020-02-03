f590a345a0db18c612a5387011ac21ae5674196b
sthtnr <sthtnr@gmail.com>
2020-02-04 02:10:47 +0900

    ✅ part4 step14

--- part4/Blog_list/controllers/blogs.js
+++ part4/Blog_list/controllers/blogs.js
@@ -37,4 +37,21 @@ blogsRouter.delete('/:id', async (request, response, next) => {
   }
 })
 
+blogsRouter.put('/:id', async (request, response, next) => {
+  const body = request.body
+
+  const blog = {
+    likes: body.likes,
+  }
+
+  try {
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+      new: true,
+    })
+    response.json(updatedBlog.toJSON())
+  } catch (e) {
+    next(e)
+  }
+})
+
 module.exports = blogsRouter
