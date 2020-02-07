6218d7a5f955bd22df569d16bd04c0927966578d
sthtnr <sthtnr@gmail.com>
2020-02-07 18:50:06 +0900

    ✅ part4 step17

--- part4/Blog_list/controllers/blogs.js
+++ part4/Blog_list/controllers/blogs.js
@@ -1,13 +1,17 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
+
+  const user = await User.findById(body.userId)
 
   if (!body.title && !body.url) {
     response.status(400).end()
@@ -18,10 +22,13 @@ blogsRouter.post('/', async (request, response, next) => {
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
+    user: user._id,
   })
 
   try {
     const result = await blog.save()
+    user.blogs = user.blogs.concat(result._id)
+    await user.save()
     response.status(201).json(result)
   } catch (e) {
     next(e)
