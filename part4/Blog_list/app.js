30c7691368c0e86c480378b20054ee116411b6eb
sthtnr <sthtnr@gmail.com>
2020-02-07 19:20:57 +0900

    ✅ part4 step18

--- part4/Blog_list/app.js
+++ part4/Blog_list/app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 
@@ -16,5 +17,6 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
