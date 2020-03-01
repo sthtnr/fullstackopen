d79bf93afd7f8037ac0222d2df6f549d1af2f9b9
sthtnr <sthtnr@gmail.com>
2020-03-01 20:09:21 +0900

    ✅ part5 step6

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,38 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [viewDetail, setViewDetail] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  if (viewDetail) {
+    return (
+      <div style={blogStyle}>
+        <div onClick={() => setViewDetail(!viewDetail)}>
+          <div>
+            {blog.title} {blog.author}
+          </div>
+          <div>{blog.url}</div>
+          <div>
+            {blog.likes} likes
+            <button>like</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  return (
+    <div style={blogStyle}>
+      <div onClick={() => setViewDetail(!viewDetail)}>
+        {blog.title} {blog.author}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
