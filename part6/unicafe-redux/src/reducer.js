f2feb4dfaacb6e59f1aa4f97a02f258ad528dca7
sthtnr <sthtnr@gmail.com>
2020-03-14 01:03:49 +0900

    ✅ part6 step1

---
+++ part6/unicafe-redux/src/reducer.js
@@ -0,0 +1,26 @@
+const initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0,
+}
+
+const objectMap = (obj, fn) =>
+  Object.fromEntries(Object.entries(obj).map(([k, v], i) => [k, fn(v, k, i)]))
+
+const counterReducer = (state = initialState, action) => {
+  console.log(action)
+  switch (action.type) {
+    case 'GOOD':
+      return { ...state, good: state.good + 1 }
+    case 'OK':
+      return { ...state, ok: state.ok + 1 }
+    case 'BAD':
+      return { ...state, bad: state.bad + 1 }
+    case 'ZERO':
+      return objectMap(state, () => 0)
+    default:
+      return state
+  }
+}
+
+export default counterReducer
