75d1c1fc4174efa3577614b60e367bcb626b320a
sthtnr <sthtnr@gmail.com>
2020-03-21 16:07:47 +0900

    ✅ part6 step8

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,28 +1,11 @@
 import React from 'react'
-import _ from 'lodash'
-import { upVote } from './reducers/anecdoteReducer'
 import NewAnec from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = props => {
-  const anecdotes = props.store.getState()
-
-  const vote = id => {
-    // console.log('vote', id)
-    props.store.dispatch(upVote(id))
-  }
-
   return (
     <div>
-      <h2>Anecdotes</h2>
-      {_.orderBy(anecdotes, 'votes', 'desc').map(anecdote => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      ))}
+      <AnecdoteList store={props.store} />
       <NewAnec store={props.store} />
     </div>
   )
---
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import _ from 'lodash'
+import { upVote } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = ({ store }) => {
+  const anecdotes = store.getState()
+
+  const vote = id => {
+    store.dispatch(upVote(id))
+  }
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {_.orderBy(anecdotes, 'votes', 'desc').map(anecdote => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default AnecdoteList
