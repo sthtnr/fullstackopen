5dc2f22fd1bddb24b8622d8d91f23aa39220db1c
sthtnr <sthtnr@gmail.com>
2020-04-03 11:17:43 +0900

    ✅ part6 step9

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -1,11 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import store from './store'
 import App from './App'
-import reducer from './reducers/anecdoteReducer'
-
-const store = createStore(reducer, composeWithDevTools())
 
 const render = () => {
   ReactDOM.render(<App store={store} />, document.getElementById('root'))
