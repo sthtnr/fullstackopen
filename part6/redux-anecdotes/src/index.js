05b2494464e1ec9a71a1bf7e77e99ef86f083253
sthtnr <sthtnr@gmail.com>
2020-04-07 10:00:39 +0900

    🌈ize with linear-gradient!

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -4,9 +4,16 @@ import { Provider } from 'react-redux'
 import store from './store'
 import App from './App'
 
+const style = {
+  background:
+    'linear-gradient(217deg, rgba(255,0,0,.8), rgba(255,0,0,0) 70.71%),linear-gradient(127deg, rgba(0,255,0,.8), rgba(0,255,0,0) 70.71%),linear-gradient(336deg, rgba(0,0,255,.8), rgba(0,0,255,0) 70.71%)',
+}
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <div style={style}>
+      <App />
+    </div>
   </Provider>,
   document.getElementById('root')
 )
