fbcf75afaa73c8dcef853c8b056b253b5608de47
sthtnr <sthtnr@gmail.com>
2020-04-24 16:28:01 +0900

    ✅ part9 step3 bmiCalculator

--- part9/part9_b/bmiCalculator.ts
+++ part9/part9_b/bmiCalculator.ts
@@ -1,3 +1,24 @@
+interface parsedObj {
+  a: number
+  b: number
+}
+
+const parseArguments = (args: Array<string>): parsedObj => {
+  if (args.length < 4) {
+    throw new Error('Not enough arguments')
+  }
+  if (args.length > 4) {
+    throw new Error('Too many arguments')
+  }
+  const a = Number(args[2])
+  const b = Number(args[3])
+  if (!isNaN(a) && !isNaN(b)) {
+    return { a, b }
+  } else {
+    throw new Error('Provided values were not numbers')
+  }
+}
+
 const calculateBmi = (height: number, weight: number): string => {
   const bmi = weight / (height / 100) ** 2
   if (bmi < 18.5) {
@@ -11,4 +32,9 @@ const calculateBmi = (height: number, weight: number): string => {
   }
 }
 
-console.log(calculateBmi(180, 74))
+try {
+  const { a, b } = parseArguments(process.argv)
+  console.log(calculateBmi(a, b))
+} catch (error) {
+  console.log(error)
+}
